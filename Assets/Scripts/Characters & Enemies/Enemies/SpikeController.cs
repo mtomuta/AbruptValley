4e46fd03e5f2d8659b88899a35bab05024af859b
mtomuta <91932333+mtomuta@users.noreply.github.com>
2021-12-28 02:12:23 +0200

    Campfire implemented in the dungeon and minor bug fixes
    - Implementación de una hoguera que solo se escucha a cierta distancia
    - Arreglo de bugs

--- Assets/Scripts/Characters & Enemies/Enemies/SpikeController.cs
+++ Assets/Scripts/Characters & Enemies/Enemies/SpikeController.cs
@@ -4,18 +4,41 @@ using UnityEngine;
 
 public class SpikeController : MonoBehaviour
 {
+    public Animator animator;
     public Attributes attributes;
     private Attacker attacker;
     protected InputEnemy input;
 
+    public float startWaitTime;
+    public float waitTime;
+
     private void Start()
     {
+        animator = GetComponent<Animator>();
         attacker = GetComponent<Attacker>();
         input = GetComponent<InputEnemy>();
+    }
+
+    void Update()
+    {
+        if (waitTime <= 0)
+        {
+            waitTime = startWaitTime;
+            animator.SetBool("attack", true);
+        }
+        else
+        {
+            waitTime -= Time.deltaTime;
+        }
     }
 
     public void SpikeDamage()
     {
         attacker.Attack(input.directionTowardsPlayer, attributes.attack);
+    }
+
+    public void SetAttackFalse()
+    {
+        animator.SetBool("attack", false);
     }
 }
